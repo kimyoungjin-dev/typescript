bbbec69ffc56c6936b9b2f5727c1751f8260c2a5
kimyoungjin-dev <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-01-15 17:33:06 +0900

    Compile the code written in index.ts in index.js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-alert("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const solution = (name, age, gender) => {
+    return `hello ${name} you are ${age} and you are ${gender}`;
+};
+console.log(solution("youngjin", 27, "mail"));
 //# sourceMappingURL=index.js.map
