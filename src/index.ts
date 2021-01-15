cccf119072489d2ab41bd0a09b1ee4187785ef5d
kimyoungjin-dev <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-01-15 17:32:29 +0900

    Make a conversion value

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const solution =(name:string, age:number, gender:string):string=>{
+  return `hello ${name} you are ${age} and you are ${gender}!`
+}
+
+console.log(solution("youngjin", 27, "mail")
+)
+
+export{} 
