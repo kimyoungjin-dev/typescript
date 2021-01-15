1385dd8a6b70b1169dfcb0e2bd4b607cf58c2dd4
kimyoungjin-dev <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-01-15 20:19:30 +0900

    Create a block chain function

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
+import *as CryptoJS from"crypto-js"
 class Block {
-  public index: number;
-  public hash: string;
-  public previousHash: string;
-  public data: string;
-  public timestamp: number;
-
-  constructor(
+public index:number;
+public hash:string;
+public previousHash:string;
+public data:string;
+public timestamp:number;
+static calculateBlockHash = (index:string, previousHash:string, timestamp:number, data:string):string=>CryptoJS.SHA256(index+previousHash+data+timestamp).toString();
+constructor(
   index:number,
   hash:string,
   previousHash:string,
   data:string,
-  timestamp:number
+  timestamp:number,
 )
-
 {
-  this.index=index;
-  this.hash=hash;
-  this.previousHash=previousHash;
-  this.data=data;
-  this.timestamp=timestamp;
-
+this.index=index;
+this.hash=hash;
+this.previousHash=previousHash;
+this.data=data;
+this.timestamp=timestamp
+}
 }
 
-}
 
-const youngjinBlock:Block = new Block(0,"1111111", "","hello", 123456)
-const blockchain: [Block]=[youngjinBlock] 
-console.log(blockchain)
-export {} 
+
+const youngjinBlock :Block = new Block(0, "202020202020", "","hello", 12345)
+let blockChain : Block[]=[youngjinBlock]
+
+console.log(blockChain)
